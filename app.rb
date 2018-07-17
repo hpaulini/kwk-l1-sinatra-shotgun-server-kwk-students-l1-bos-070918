10a0fbc8ab075a0474bacead18f7ed4ba0b3aedb
hpaulini <helenpaulini@gmail.com>
2018-07-17 15:47:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,19 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! I am trying to make a change. This is where shotgun comes in handy."
+  end
+  
+  get '/name' do
+    "My name is Helen."
+  end
+  
+  get '/boy-time' do
+    "It is now boy time."
+  end
+  
+  get '/karlie-is-coming-again' do
+    "hello world!"
   end
 
 end
